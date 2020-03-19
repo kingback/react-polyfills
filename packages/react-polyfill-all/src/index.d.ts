a61dc2d832b8dbe43efa9659fe20be241c98ace5
huya.nzb <huya.nzb@taobao.com>
2020-03-19 17:28:52 +0800

    feat: add index.d.ts

---
+++ packages/react-polyfill-all/src/index.d.ts
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export const createRef: typeof React.createRef;
+export const forwardRef: typeof React.forwardRef;
+export const createContext: typeof React.createContext;
+
+export const withHooks: (render: React.FC) => React.Component;
+export const useRef: typeof React.useRef;
+export const useState: typeof React.useState;
+export const useContext: typeof React.useContext;
+export const useReducer: typeof React.useReducer;
+export const useEffect: typeof React.useEffect;
+export const useLayoutEffect: typeof React.useLayoutEffect;
+export const useMemo: typeof React.useMemo;
+export const useCallback: typeof React.useCallback;
+export const useImperativeHandle: typeof React.useImperativeHandle;
+export const useDebugValue: typeof React.useDebugValue;
